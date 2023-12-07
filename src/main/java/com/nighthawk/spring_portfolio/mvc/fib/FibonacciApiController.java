f34321a83aa15531532e5e29e8efade57fe83538
dolphinalt <ethan.zhao.us@gmail.com>
2023-12-07 09:42:49 -0800

    add time to return

--- src/main/java/com/nighthawk/spring_portfolio/mvc/fib/FibonacciApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/fib/FibonacciApiController.java
@@ -24,57 +24,61 @@ public class FibonacciApiController {
 
     @PostMapping("/fibFor")
     public ResponseEntity<ArrayList<Integer>> getFibonacciFor(@RequestBody FibRequest request) {
-        int length = request.getLength();
-
         int startTime = (int)System.currentTimeMillis();
-        FibonacciFor fibonacciFor = new FibonacciFor();
-        int endTime = (int)System.currentTimeMillis();
-        int elapsedTime = endTime - startTime;
 
+        int length = request.getLength();
+        FibonacciFor fibonacciFor = new FibonacciFor();
         ArrayList<Integer> fibonacciSeries = fibonacciFor.FibonacciFor(length);
         System.out.println(fibonacciFor.toStringArr(fibonacciSeries));
+        int endTime = (int)System.currentTimeMillis();
+        int elapsedTime = endTime - startTime;
+        fibonacciSeries.add(elapsedTime);
+
         return new ResponseEntity<>(fibonacciSeries, HttpStatus.OK);
     }
 
     @PostMapping("/fibRecursive")
     public ResponseEntity<ArrayList<Integer>> getFibonacciRecursive(@RequestBody FibRequest request) {
-        int length = request.getLength();
-
         int startTime = (int)System.currentTimeMillis();
-        FibonacciRecursive fibonacciRecursive = new FibonacciRecursive();
-        int endTime = (int)System.currentTimeMillis();
-        int elapsedTime = endTime - startTime;
 
+        int length = request.getLength();
+        FibonacciRecursive fibonacciRecursive = new FibonacciRecursive();
         ArrayList<Integer> fibonacciSeries = fibonacciRecursive.FibonacciRecursive(0, length);
         System.out.println(fibonacciRecursive.toStringArr(fibonacciSeries));
+        int endTime = (int)System.currentTimeMillis();
+        int elapsedTime = endTime - startTime;
+        fibonacciSeries.add(elapsedTime);
+
         return new ResponseEntity<>(fibonacciSeries, HttpStatus.OK);
     }
 
     @PostMapping("/fibStream")
     public ResponseEntity<ArrayList<Integer>> getFibonacciStream(@RequestBody FibRequest request) {
-        int length = request.getLength();
-
         int startTime = (int)System.currentTimeMillis();
-        FibonacciStream fibonacciStream = new FibonacciStream();
-        int endTime = (int)System.currentTimeMillis();
-        int elapsedTime = endTime - startTime;
 
+        int length = request.getLength();
+        FibonacciStream fibonacciStream = new FibonacciStream();
         ArrayList<Integer> fibonacciSeries = fibonacciStream.FibonacciStream(length);
         System.out.println(fibonacciStream.toStringArr(fibonacciSeries));
+        int endTime = (int)System.currentTimeMillis();
+        int elapsedTime = endTime - startTime;
+        fibonacciSeries.add(elapsedTime);
+
         return new ResponseEntity<>(fibonacciSeries, HttpStatus.OK);
     }
 
     @PostMapping("/fibWhile")
     public ResponseEntity<ArrayList<Integer>> getFibonacciWhile(@RequestBody FibRequest request) {
-        int length = request.getLength();
-
         int startTime = (int)System.currentTimeMillis();
-        FibonacciWhile fibonacciWhile = new FibonacciWhile();
-        int endTime = (int)System.currentTimeMillis();
-        int elapsedTime = endTime - startTime;
 
+        int length = request.getLength();
+        FibonacciWhile fibonacciWhile = new FibonacciWhile();
         ArrayList<Integer> fibonacciSeries = fibonacciWhile.FibonacciWhile(length);
         System.out.println(fibonacciWhile.toStringArr(fibonacciSeries));
+        int endTime = (int)System.currentTimeMillis();
+        int elapsedTime = endTime - startTime;
+        fibonacciSeries.add(elapsedTime);
+
         return new ResponseEntity<>(fibonacciSeries, HttpStatus.OK);
     }
 }
